137149f1a15330cbd2cf320daade049574f432d1
Markel Rubiños <46656741+markelrlopez@users.noreply.github.com>
2023-10-22 19:12:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://your-frontend.com"
+    "https://markelrlopez.com"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
